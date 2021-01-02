c60dfaa4b6c4fa9c93e19cb915ee3fc2af93a9fe
kudpig <shin.kudo.aa@gmail.com>
2021-01-02 11:32:08 +0900

    Initial Commit

---
+++ prototype/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  prototype
+//
+//  Created by shinichiro kudo on 2021/01/02.
+//  Copyright © 2021 shinichiro kudo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
